c3f5c92773d89765df030c3529a84c4bd3b0f40b
Zaid <itzzaid@pm.me>
2022-08-21 09:36:21 +0530

    Update zaid.py

--- zaid.py
+++ zaid.py
@@ -12,8 +12,6 @@ API_ID = "6435225"
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 TOKEN = [i.strip() for i in os.environ.get("TOKEN").split(' ')]
 
-ZAID = Client("ZPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
-
 for phone in TOKEN:
     try:
         client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "handlers"})
