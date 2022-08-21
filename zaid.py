a6566ab6db87a94a17118c8d02b40953b2ae995f
Zaid <itzzaid@pm.me>
2022-08-21 09:35:43 +0530

    Update zaid.py

--- zaid.py
+++ zaid.py
@@ -14,33 +14,11 @@ TOKEN = [i.strip() for i in os.environ.get("TOKEN").split(' ')]
 
 ZAID = Client("ZPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 
-
-@ZAID.on_message(filters.private & filters.command("start"))
-async def hello(client: ZAID, message: Message):
-    await message.reply("Hey! It's Just a Cloner Bot example source Code")
-
-##Copy from here 
-
-# © By Itz-Zaid Your motherfucker if uh Don't gives credits.
-@ZAID.on_message(filters.private & filters.command("clone"))
-async def clone(bot: ZAID, msg: Message):
-    chat = msg.chat
-    text = await msg.reply("Usage:\n\n /clone token")
-    cmd = msg.command
-    phone = msg.command[1]
+for phone in TOKEN:
     try:
-        await text.edit("Booting Your Client")
-                   # change this Directry according to ur repo
         client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "handlers"})
-        await client.start()
-        idle()
-        user = await client.get_me()
-        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @Amala_music_assistant_1 To Your Chat!\n\nThanks for Cloning.")
+        client.start()
     except Exception as e:
-        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
-#End
-##This code fit with every pyrogram Codes just import then @Client Xyz!
+        print(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
 
-
-ZAID.start()
 idle()
